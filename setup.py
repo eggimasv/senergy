37ff2a7f6a5b6fba363090520c22fa47c01e9390
Sven Eggimann <sven.eggimann@ouce.ox.ac.uk>
2019-03-14 14:14:12 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Setup file for senergy.
+    Use setup.cfg to configure your project.
+
+    This file was generated with PyScaffold 3.1.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
+"""
+import sys
+
+from pkg_resources import require, VersionConflict
+from setuptools import setup
+
+try:
+    require('setuptools>=38.3')
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="synergy",
+    version="v0.0.6",
+    author="Example Author",
+    author_email="sven.eggimann@ouce.ox.ac.uk",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eggimasv/senergy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+if __name__ == "__main__":
+    setup(use_pyscaffold=True)
