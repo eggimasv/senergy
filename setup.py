0575d938ff1a5629494caf42380ed187dcf1829b
Sven Eggimann <sven.eggimann@ouce.ox.ac.uk>
2019-03-18 12:15:21 +0000

    updated requirements

--- setup.py
+++ setup.py
@@ -24,22 +24,6 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="synergy",
-    version="v0.0.8",
-    author="Example Author",
-    author_email="sven.eggimann@ouce.ox.ac.uk",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/eggimasv/senergy",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
 
 if __name__ == "__main__":
     setup(use_pyscaffold=True)
