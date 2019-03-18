9e8f2cb6be3613c78cf81c0cfabd5fd41bc21348
Sven Eggimann <sven.eggimann@ouce.ox.ac.uk>
2019-03-18 14:03:52 +0000

    updated 2+

--- setup.py
+++ setup.py
@@ -21,9 +21,58 @@ except VersionConflict:
 
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def readme():
+    """Read README contents
+    """
+    with open('README.md') as f:
+        return f.read()
 
+setup(
+    name='senergy',
+    version='0.1.2',
+    license='MIT License',
+    description='description',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author='Sven Eggimann',
+    author_email='sven.eggimann@gmail.com',
+    url='https://github.com/eggimasv/senergy',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Utilities',
+    ],
+    keywords=[
+        # eg: 'keyword1', 'keyword2', 'keyword3',
+    ],
+    install_requires=[
+        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        'fiona>=1.7.13',
+        'shapely>=1.6',
+        'geopandas==0.4.0',
+        'rtree>=0.8'
+    ],
+    extras_require={
+        # eg:
+        #   'rst': ['docutils>=0.11'],
+        #   ':python_version=="2.6"': ['argparse'],
+    },
+    entry_points={
+        'console_scripts': [
+            # eg: 'snkit = snkit.cli:main',
+        ]
+    },
+)
 
 if __name__ == "__main__":
     setup(use_pyscaffold=True)
